33476b80fa9d9b68db44c590a661551b4315af19
Supavitch Ponak <supavitch@192.168.0.106>
2023-09-26 01:18:19 +0700

    finished exercise

--- ex-4.js
+++ ex-4.js
@@ -2,3 +2,25 @@
 
 // Start coding here
 let calculator = {};
+
+calculator.add = function (a, b) {
+    return a + b;
+};
+
+calculator.subtract = function (a, b) {
+    return a - b;
+};
+
+calculator.multiply = function (a, b) {
+    return a * b;
+};
+
+calculator.divide = function (a, b) {
+    return a / b;
+};
+
+const resultAdd = calculator.add(10 , 20);
+console.log(resultAdd);
+
+const resultDivide = calculator.divide(3000, 10);
+console.log(resultDivide);
