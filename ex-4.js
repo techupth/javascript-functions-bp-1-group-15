d0c5fdcac9d83edadd2b8a722d627305b26bc67c
pranaiya suwanta <27mar03.jk@gmail.com>
2023-09-27 14:00:41 +0700

    finished assignment

--- ex-4.js
+++ ex-4.js
@@ -2,3 +2,21 @@
 
 // Start coding here
 let calculator = {};
+
+calculator.add = (a,b) => {
+    return a+b;
+};
+calculator.subtract = (a,b) =>{
+    return a-b;
+};
+calculator.multiply = (a,b) =>{
+    return a*b;
+};
+calculator.divide = (a,b) =>{
+    return a/b;
+};
+let resultAdd = calculator.add(10,20);
+console.log(resultAdd);
+let resultDivide = calculator.divide(3000,10);
+console.log(resultDivide);
+
--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,7 @@
 // Exercise #5 Min Function
 // Start coding here
-const minNumber = findMinNumber(10, 20);
-
+function findMinNumber(a,b){
+    return Math.min(a,b);
+};
+const minNumber = findMinNumber(10, 20)
 console.log(minNumber); // 10
